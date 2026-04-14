748fc896e45e2e3a58f4a47f227948b127e2cb7c
perezvozhin <sandakov.04@mail.ru>
2026-04-15 04:11:14 +0500

    name refactor

--- Database/ApplicationDbInit.cs
+++ Database/ApplicationDbInit.cs
@@ -8,7 +8,7 @@ public class ApplicationDbInit : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
-    public DbSet<session> sessions { get; set; }
+    public DbSet<Sessions> sessions { get; set; }
 
     public ApplicationDbInit(DbContextOptions<ApplicationDbInit> options) : base(options)
     {
@@ -18,6 +18,7 @@ public class ApplicationDbInit : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
+            
             .HasKey(u => u.id);
     }
 }
--- Models/Sessions.cs
+++ Models/Sessions.cs
@@ -1,6 +1,6 @@
 ﻿namespace WebApplication3.Models;
 
-public class session
+public class Sessions
 {
     public int userid { get; set; }
     public string token { get; set; }
