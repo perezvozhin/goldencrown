924302026db62148c86b9933a2023048b1039a56
perezvozhin <sandakov.04@mail.ru>
2026-04-14 23:05:15 +0500

    init with EF

---
+++ Database/ApplicationDbInit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Database;
+
+public class ApplicationDbInit : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<session> sessions { get; set; }
+
+    public ApplicationDbInit(DbContextOptions<ApplicationDbInit> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+    }
+}
